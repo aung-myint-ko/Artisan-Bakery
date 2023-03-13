935d240ae842dbe0a839212f70f56f890adb1fd8
aung-myint-ko <aungmyintkoanarmala@gmail.com>
2023-03-13 21:35:07 +0630

    changing api url

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -2,7 +2,6 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const axiosInstance = axios.create({
-  // baseURL: "https://artisan-bakery-data.onrender.com/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://artisan-bakery-data.onrender.com/api",
 });
 export default axiosInstance;
